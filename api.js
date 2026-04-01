1342ad7969a71b5bb99e4bfd4da32c2613f4c89f
Vane <Vane@DESKTOP-VANERIM>
2026-03-31 22:39:17 -0300

    primeras funciones

---
+++ api.js
@@ -0,0 +1,66 @@
+const url = 'https://thronesapi.com/api/v2/Characters';
+
+
+async function getPersonajes(){ // a) Recuperar la información de todos los personajes (GET).
+   try{
+    const respuesta = await fetch(url);
+    if(!respuesta.ok){ //si no devuelve un status 200, entonces es un error
+         console.log(`Error!`);
+    }else{ //si no es error, entonces devuelve la respuesta en formato json
+        //immprimimos la respuesta
+        console.log("Todos los personajes:");
+        const personajes = await respuesta.json();
+        console.log(personajes);
+    }
+   }catch(error){
+    console.log(`Error! ${error}`); 
+  }
+} 
+
+
+
+async function agregarPersonaje(personaje){ // b) Agregar un nuevo personaje (POST).
+    try{
+         const post = await fetch( url, { //la url es la misma pero le indicamos que el metood es post
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(personaje)
+        })
+
+        if (!post.ok){
+            console.log(`Error!`);
+        }else{
+        //imprimimos la respuesta
+        console.log("Respuesta al insertar nuevo personaje:");
+        const respuesta = await post.status;
+        console.log(respuesta);
+        }
+    }
+    catch(error){
+    console.log(`Error! ${error}`);   
+ }
+}
+
+
+
+async function buscarPersonaje(id){ // c) Buscar la información de un determinado personaje, utilizando un “id” como parámetro (GET).
+    try{
+        const respuesta = await fetch(`${url}/${id}`);
+        if (!respuesta.ok){
+            console.log(`Error!`);
+        }else{
+             console.log("Personaje encontrado:");
+            const personaje = await respuesta.json();
+            console.log(personaje);
+        }
+    }catch(error){
+        console.log(`Error! ${error}`);
+    }
+}
+module.exports = {
+    getPersonajes,
+    agregarPersonaje,
+    buscarPersonaje 
+}
---
+++ inicio.js
@@ -0,0 +1,48 @@
+/*
+Consigna
+    API Fecth – File System
+    1 - Utilizando el API https://thronesapi.com/ realizar las siguientes tareas:
+     swagger (https://thronesapi.com/swagger/index.html?urls.primaryName=Game%20of%20Thrones%20API%20v2)
+    a) Recuperar la información de todos los personajes (GET).
+    b) Agregar un nuevo personaje (POST).
+    c) Buscar la información de un determinado personaje, utilizando un “id” como parámetro
+    (GET).
+    d) Persistir los datos de la primer consulta en un archivo local JSON.
+    Métodos comunes y avanzados – File System
+
+    
+*/
+var api = require ('./api.js');
+//a) Recuperar la información de todos los personajes (GET).
+api.getPersonajes();
+
+// b) Agregar un nuevo personaje (POST).
+const nuevoPersonaje = {
+    "id": 99,
+    "firstName": "Harry", 
+    "lastName": "potter",
+    "fullName": "El niño que vivió",
+    "title": "mago",
+    "family": "huerfano",
+    "image": "harry.jpg",
+    "imageUrl": "https://thronesapi.com/assets/images/harry.jpg"
+}  
+
+api.agregarPersonaje(nuevoPersonaje);
+// c) Buscar la información de un determinado personaje, utilizando un “id” como parámetro (GET).
+api.buscarPersonaje(1);
+
+
+/**2 - Utilizando el archivo creado en el punto anterior:
+    a) Agregar un personaje al final del archivo.
+    b) Agregar dos personajes al inicio del archivo.
+    c) Eliminar el primer personaje, mostrar en consola el elemento eliminado.
+    d) Crear un nuevo archivo que solo contenga los: id y nombres de los personajes.
+    e) Para los datos anteriores ordenar por nombre y de forma decreciente, luego mostrar por
+    consola (investigar método sort()).
+    
+    Imprimir en consola para verificar todas las operaciones realizadas.
+    Tener en cuenta:
+     Realizar funciones individuales para cada punto.
+     Utilizar sintaxis Aync/Await, la forma más moderna para el uso de la asincronía.
+     Manejar de excepciones, bloques try...catch */
